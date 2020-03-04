6211d031c61a227c6b05dc2c2a52dd305c567461
Spencer Axford <saxford@mun.ca>
2020-03-03 21:55:25 -0330

    Moved bluetooth code into interfaces

--- bluetooth/nrf52/NRF52RxTx.h
+++ bluetooth/nrf52/NRF52RxTx.h
@@ -1,8 +1,19 @@
 #include "ChirpBluetoothRxTx.h"
+#include <bluefruit.h>
+
+// Function prototypes for packetparser.cpp
+uint8_t readPacket (BLEUart *ble_uart, uint16_t timeout);
+float   parsefloat (uint8_t *buffer);
+void    printHex   (const uint8_t * data, const uint32_t numBytes);
+
+// Packet buffer
+extern uint8_t packetbuffer[];
 
 class NRF52RxTx: public ChirpBluetoothRxTx
 {
     public:
+        NRF52RxTx();
+        void init();
         void send(char[], int);
         char* receive();
 };
