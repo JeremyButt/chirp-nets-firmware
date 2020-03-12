fb50427df4cc29a84978f3d9c39825c71d055edd
delightedwaif <tim@milkmoovement.com>
2020-03-12 11:40:32 -0230

    add most recent code for john

--- combined/RFM69RxTx.cpp
+++ combined/RFM69RxTx.cpp
@@ -41,7 +41,7 @@ void RFM69RxTx::send(int toAddress, char payload[], size_t len)
 
         if (Serial)
             Serial.println("Sending");
-        if (radio.sendWithRetry(toAddress, payload, len, 5, 2000))
+        if (radio.sendWithRetry(toAddress, payload, len, 10, 2000))
         {
             if (Serial)
                 Serial.println("ACK received");
