9f091f5893861b56d427775743b4a5acfbb651cd
delightedwaif <tim@milkmoovement.com>
2020-03-16 09:52:38 -0230

    remove unused folders

--- bluetooth/nrf52/ChirpBluetoothRxTx.h
+++
@@ -1,6 +0,0 @@
-class ChirpBluetoothRxTx
-{
-    public:
-        virtual void send(char[], size_t) = 0;
-        virtual char* receive() = 0;
-};
--- radio/rfm69/ChirpFMRxTx.h
+++
@@ -1,7 +0,0 @@
-class ChirpFMRxTx
-{
-    public:
-        virtual void send(int, char[], size_t) = 0;
-        virtual char* receive() = 0;
-        virtual int getToAddress(char[]) = 0;
-};
