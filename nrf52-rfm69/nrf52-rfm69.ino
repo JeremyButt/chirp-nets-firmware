a9fb72041568a630061dcf7d06f259c4734c8eab
Spencer Axford <saxford@mun.ca>
2020-03-28 20:42:15 -0230

    Removed commented out, old code

--- nrf52-rfm69/nrf52-rfm69.ino
+++ nrf52-rfm69/nrf52-rfm69.ino
@@ -34,21 +34,4 @@ void loop()
 	{
 		bluetooth.send(receivedData, strlen(receivedData));
 	}
-  // char packet[122] = {NULL};
-  // blue_io.receive(packet);
-  // if (packet[0])
-  // {
-  //   Serial.println("Sending over radio");
-  //   Serial.println(packet);
-  //   radio_io.send(2, packet, sizeof(packet));
-  // }
-
-  // char payload[122] = {NULL};
-  // radio_io.receive(payload);
-  // if (payload[0])
-  // {
-  //   Serial.println("Sending to phone from radio");
-  //   Serial.println(payload);
-  //   blue_io.send(payload, sizeof(payload));
-  // }
 }
